a25dd92984132b881f3b3fb068184c231999de91
Lenochka <lvutina.en@gmail.com>
2022-04-19 19:06:30 +0400

    feat: add health history year and condition form

--- src/features/patient/intake/PatientIntakeHealthHistory.tsx
+++ src/features/patient/intake/PatientIntakeHealthHistory.tsx
@@ -1,28 +1,40 @@
 import * as React from 'react';
-import { useSelector } from 'react-redux';
+import {useSelector} from 'react-redux';
 
 import {
   Collection,
   PatientHealthHistory
 } from 'documents';
-import { Routes } from 'routes';
+import {Routes} from 'routes';
 
-import {  useDocument } from 'hooks/firestore';
+import {useDocument} from 'hooks/firestore';
 
-import { RootState } from 'app/rootReducer';
-import { Page } from 'components';
+import {RootState} from 'app/rootReducer';
+import {Page, WizardNavigation} from 'components';
+import {Form, Input} from "antd";
+import {
+  HealthHistoryForm
+} from "../../../components/health-history-form/HealthHistoryForm";
+
 
 export const PatientIntakeHealthHistory: React.FC = () => {
   const user = useSelector((state: RootState) => state.auth.user);
   const {
     hasError,
     isLoading,
-    
   } = useDocument<PatientHealthHistory>({
     path: Collection.PatientHealthHistories,
     id: user!.uid
   });
-
+  const [form] = Form.useForm();
+  const handleValuesChange = React.useCallback(
+    (changedValue: any) => {
+      if (Object.keys(changedValue)[0] === 'cancer') {
+        form.resetFields();
+      }
+    },
+    []
+  );
 
 
   return (
@@ -32,7 +44,41 @@ export const PatientIntakeHealthHistory: React.FC = () => {
       hasError={hasError }
       backLink={Routes.PatientIntake}
     >
-      
+      <p>
+        Have you ever been diagnosed with any of the following:
+      </p>
+
+        < HealthHistoryForm labels={["Health Issue", "Answer", "Year of onset"]}
+                            values={['Asthma',
+                              'Chronic Obstructive Pulmonary Disease',
+                              'Chron’s Disease',
+                              'Chronic Pancreatitis',
+                              'Type 1 Diabetes',
+                              'Type 2 Diabetes',
+                              'High Blood Sugar',
+                              'High Blood Pressure',
+                              'High Cholesterol',
+                              'Cancer']}
+        />
+      <Form
+        form={form}
+        name="health-history-form-comment"
+        layout="vertical"
+        onValuesChange={handleValuesChange}
+      >
+        <Form.Item className="hh-comments" name="comments" label="Comments">
+          <Input.TextArea
+            rows={5}
+            placeholder="Please add any details you consider important."
+          />
+        </Form.Item>
+      </Form>
+      <WizardNavigation
+        back={Routes.PatientIntakeProfile}
+      />
+      <WizardNavigation
+        next={Routes.PatientIntakeHealthHistoryAllergies}
+      />
     </Page>
   );
 };
